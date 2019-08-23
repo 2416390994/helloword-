0cb1273df6603e9f8e6543e26dbcb12960db408a
2416390994 <2416390994@qq.com>
2019-08-23 22:14:06 +0800

    8_23.cpp

---
+++ 8_23.cpp
@@ -0,0 +1,60 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int add = 0;
+	cout << a << endl << b << endl;
+	int count = 2;
+	for (count; count <= 10; ++count)
+	{
+		add = a + b;
+		cout << add << endl;
+		a = b;
+		b = add;
+	}
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+void test(int a,int b,int count)
+{
+	
+	if (count-- == 0)
+	{
+		return;
+	}
+	int c = a + b;
+	cout << c << endl;
+	test(b, c, count);
+}
+int main()
+{
+
+	test(1,2,8);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
