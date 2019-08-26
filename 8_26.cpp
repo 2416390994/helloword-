a4c7f9000bb85f3a24be755e6d0d439248313dc2
2416390994 <2416390994@qq.com>
2019-08-26 23:36:02 +0800

    8_26.cpp

---
+++ 8_26.cpp
@@ -0,0 +1,28 @@
+/*
+#include<iostream>
+using namespace std;
+void func(void)
+{
+	unsigned int a = 6;
+	int b = -20;
+	cout << a*b;
+}
+int main()
+{
+	func();
+	return 0;
+}
+*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+void func(double)
+{
+	cout << "fehwieuhf" << endl;
+}
+int main()
+{
+	int a = 2;
+	func(a);
+	return 0;
+}
