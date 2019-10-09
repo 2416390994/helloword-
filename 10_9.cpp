712882293321eefb12fb99b755fdd5a6ed88816f
2416390994 <2416390994@qq.com>
2019-10-09 22:05:30 +0800

    10_9.cpp

---
+++ 10_9.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	//char* a = "132";
+	//int d;
+	//sscanf(a, "%d", &d);
+	//cout << d << endl;
+	//int c = atoi(a);
+	//cout << c * 2 << endl;
+	//string s = "123465789123456456111111111111111111111111111111111111111111111111111111111111";
+	////cin >> s;
+	////cout<<s.size() << endl;
+	//s.reserve(10);
+	//cout << s.capacity() << endl;
+	 //const char* s = "13465";
+	//cout << s[1] << endl;
+	/*string a = "123456";
+	int c = a.find('2');
+	cout << c << endl;
+	cout << a[c + 2] << endl;*/
+	//string url("http://www.cplusplus.com/reference/string/string/find/");
+	//size_t start = url.find("://");
+	///*if (start == string::npos)
+	//{
+	//	cout << "invalid url" << endl;
+	//	return;
+	//}*/
+	//start += 3;
+	//size_t finish = url.find('/', start);
+	//cout << url[finish];
+	//string address = url.substr(start, finish - start);
+	//cout << address << endl;
+	string b = "abc";
+	int a[256] = { 0 };
+	b[a[0]]++;
+
+	return 0;
+}
