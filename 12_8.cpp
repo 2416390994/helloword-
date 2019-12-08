d4bd61481ddd670d2757372b841cbe278df7c105
2416390994 <2416390994@qq.com>
2019-12-08 21:01:28 +0800

    12_8.cpp

---
+++ 12_8.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void swap_s(int* a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{
+	//int a[10];
+	//int count = 0;
+	//int n;
+	//scanf("%d", &n);
+	//for (int i = 0; i < n; ++i)
+	//{
+	//	scanf("%d", &a[i]);
+	//	count += a[i];
+	//}
+	//count /= n;
+	//for (int i = 0; i < 10; ++i)
+	//{
+	//	if (a[i] > count)
+	//		printf("%d  ", a[i]);
+	//}
+	//printf("\n");
+	//return 0;
+	
+}
