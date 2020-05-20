a466b89c9e356bf6b16b0caff9dc95e1196ed3f9
2416390994 <2416390994@qq.com>
2020-05-20 23:54:37 +0800

    5_18.cpp

---
+++ 5_18.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include <thread>
+void fun()
+{
+	cout << "A new thread!" << endl;
+}
+int main()
+{
+	/*int b = 10;
+	int c = 20;
+	const int*  a = &b;
+	*a = 30;
+	a = &c;
+	cout << *a << endl;*/
+
+	//char* a = "abc";
+	//cout << a << endl;
+	//thread t(fun);
+	//t.join();
+	//cout << "Main thread!" << endl;
+	////t.join();
+	return 0;
+}
+
+
